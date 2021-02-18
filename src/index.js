4c36a39a1f10b745db06d870ecee5d5970a94c84
Juli R <julirg58@gmail.com>
2021-02-18 00:15:45 -0500

    dev branch created

--- src/index.js
+++ src/index.js
@@ -8,5 +8,5 @@ const app = express();
 app.use(express.json());
 
 app.listen(PORT,() => {
-    console.log("Servidor corriendo en el puerto : ",PORT);
+    console.log("Server runing in : ",PORT);
 });
