ef12889b02ebf5ebcaca976a42ed32558c89242c
Edgar <johndoe@example.com>
2021-02-18 21:13:25 -0600

    sequelize v1

---
+++ src/models/resettokens.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class ResetTokens extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  ResetTokens.init({
+    token: DataTypes.STRING,
+    expirationDate: DataTypes.DATE,
+    userId: DataTypes.INTEGER,
+    active: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'ResetTokens',
+  });
+  return ResetTokens;
+};
