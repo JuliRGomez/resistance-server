4f9d975211fb42113fc74a85516a0c882109bbc3
Juli R <julirg58@gmail.com>
2021-02-19 23:30:41 -0500

    actualizacion

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRouter from "./routes/auth";
+import cors from "cors";
 
 dotenv.config();
 const PORT = process.env.PORT || 8000;
+
+app.use(cors());
+
+app.use("/api/v1",authRouter)
 
 const app = express();
 app.use(express.json());
